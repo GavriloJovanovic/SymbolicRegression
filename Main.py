dbab07f61d91b08cb0f95fb002f72d226c85e884
GavriloJovanovic <57016056+GavriloJovanovic@users.noreply.github.com>
2021-12-28 23:11:17 +0100

    Mutation of nodes added
    Refactorization of Node class

--- Main.py
+++ Main.py
@@ -8,11 +8,25 @@ import Node
 
 try:
 
-    iteration = 0
     drvo = Node.Tree(0,5,0)
-    drvo.generateSubTree(None,5,5,1,iteration)
+    drvo.generateSubTree(None,5,5,1,True)
+    print("Prvi ispis, kako nam izgleda nasumicno drvo koje je napravljeno")
     print(drvo.modificationTree.stringNode())
+    print("Prvi ispis, koja je vrednost drveta za x = 1")
     print(drvo.modificationTree.getValue(1))
+    drvo.setTree(drvo.getModTree())
+    print("Drugi ispis, postavli smo da je drvo modifikovano stablo iz generisanog sub tree")
+    print(drvo.tree.stringNode())
+    print("-------------------------------")
+    drvo.mutation(0.3)
+    print("Drvo posle mutacije")
+    print(drvo.tree.stringNode())
+    print(drvo.tree.getValue(1))
+    print("---------------------------------------------")
+    drvo.mutation(0.3)
+    print("Drvo posle druge mutacije")
+    print(drvo.tree.stringNode())
+    print(drvo.tree.getValue(1))
 
 except IOError:
     print("Greska")
--- Node.py
+++ Node.py
@@ -1,12 +1,13 @@
 from enum import Enum
 import random
 import numpy as np
-from abc import ABC, abstractmethod
 
 class Type(Enum):
+    FIRST = 0
     TERM = 1
     OPERATOR = 2
     TRIGONOMETRY = 3
+
 
 class Node:
     def __init__(self,type,level,char = None,value=None):
@@ -16,8 +17,13 @@ class Node:
         self.level = level
         self.child1 = None
         self.child2 = None
+        self.MUTATION_RATE = 0 #We use this parametar to determan mutation rate of node
 
+    def getMutationRate(self):
+        return self.MUTATION_RATE
 
+    def setMutationRate(self,num):
+        self.MUTATION_RATE = num
 
     def setChild1(self,node):
         self.child1 = node
@@ -29,6 +35,8 @@ class Node:
         return self.type
 
     def stringNode(self):
+        if self.type == Type.FIRST:
+            return self.child1.stringNode()
         if self.type == Type.TERM:
             return self.char
         elif self.type == Type.TRIGONOMETRY:
@@ -36,8 +44,47 @@ class Node:
         else:
             return "( " + self.child1.stringNode() + " " + self.char + " " + self.child2.stringNode() + " )"
 
+    def mutate(self,Tree):
+        # We are mutationg FIRST,TRIGONOMETRY and OPERATORS nodes becouse
+        # we want to change our node, and subsequently our subtree
+
+        #If our node is FIRST or TRIGONOMETRY we are only applying mutation on
+        #first child
+        if self.type == Type.FIRST or self.type == Type.TRIGONOMETRY:
+            randomMutation = random.random()
+            if randomMutation < self.getMutationRate():
+                Tree.generateSubTree(None,3,3,1)
+                self.child1 = Tree.getModTree()
+                #We are allways setting setMutationRate on 0 after we succesfully mutate a node
+                self.setMutationRate(0)
+            else:
+                self.child1.setMutationRate(self.getMutationRate())
+                self.child1.mutate(Tree)
+
+        elif self.type == Type.OPERATOR:
+            randomMutation = random.random()
+            if randomMutation < self.getMutationRate():
+                Tree.generateSubTree(None,3,3,1)
+                self.child1 = Tree.getModTree()
+                self.setMutationRate(0)
+            else:
+                self.child1.setMutationRate(self.getMutationRate())
+                self.child1.mutate(Tree)
+
+            randomMutation = random.random()
+            if randomMutation < self.getMutationRate():
+                Tree.generateSubTree(None,3,3,1)
+                self.child2 = Tree.getModTree()
+                self.setMutationRate(0)
+            else:
+                self.child2.setMutationRate(self.getMutationRate())
+                self.child2.mutate(Tree)
+
+
     def getValue(self,xValue):
-        if self.type == Type.TERM:
+        if self.type == Type.FIRST:
+            return self.child1.getValue(xValue)
+        elif self.type == Type.TERM:
             if self.char == "x":
                 return xValue
             return self.value
@@ -57,7 +104,8 @@ class Node:
                 if self.child2.getValue(xValue) != 0:
                     return self.child1.getValue(xValue) / self.child2.getValue(xValue)
                 else:
-                    exit(-1,"Greska")
+                    exit(-1)
+
 
 
  #   @abstractmethod
@@ -69,9 +117,11 @@ class Tree:
         self.value = 0
         self.maxDepth = maxDepth
         self.goals = goals # that is array of vectors
-        self.tree = None
-        self.modificationTree = None
+        self.tree = None # first is just one node, but it needs to be array of nodes
+        self.modificationTree = None #this is special modificiation node for making new subtrees
 
+
+    #racuna fitness, jos nepotreban
     def calculateFitness(self):
         err = 0
         testLength = len(self.goals)
@@ -84,9 +134,18 @@ class Tree:
     def setModificationTreeOnNone(self):
         self.modificationTree = None
 
+
+    #We use mutation on one of our's roots nodes!
+    def mutation(self,mutationRate):
+        #Setting mutation rate in node
+        self.tree.setMutationRate(mutationRate)
+        # recusrsive method mutation
+        self.tree.mutate(self)
+
+
+    # funkcija koja generise nasumican cvor
     def generateRandomNode(self, depth, xValue):
         randType = None
-        #print("I am generating a random number for depth: " + str(depth) )
         if depth == 1:
             randType = Type.TERM
         elif depth == 2:
@@ -100,7 +159,7 @@ class Tree:
         elif randType == Type.TRIGONOMETRY:
             randomTrig = random.choice(['cos','sin'])
             return Node(Type.TRIGONOMETRY,-1,randomTrig)
-        else:
+        elif randType == Type.TERM:
             randNumberTerm = random.random()
             if randNumberTerm < 0.5:
                 randomTermNumber = round(random.random() * 10, 2)
@@ -108,11 +167,17 @@ class Tree:
             else:
                 return Node(Type.TERM, -1, 'x', xValue)
 
-    # function that generate random subtree with <=depth elements
-    # Before we call this function we must set modificationTree on None!
-    def generateSubTree(self,currentNode,depth,nodeNum,xValue,iteration):
-        #if currentNode != None:
-            #print(currentNode.char + " " + str(iteration))
+    def setTree(self,node):
+        self.tree = node
+
+    def getModTree(self):
+        return self.modificationTree
+
+
+    #funkcija koje generise nasumicno podstablo, pre nje moramo da kazemo
+    #current node je trenutan cvor, depth je dubina cvora da znamo kako da konstruisemo nasumicni cvor,
+    #nodeNum je slican broj, ignorisi ga a xValue je vrednost X-a koji prosledjujemo za test prvo
+    def generateSubTree(self,currentNode,depth,nodeNum,xValue,areWeGenerateFullTree = False):
 
         if currentNode != None and currentNode.type == Type.TERM:
             return
@@ -121,15 +186,20 @@ class Tree:
             return
 
 
-        if self.modificationTree == None:
-            self.modificationTree = self.generateRandomNode(depth,xValue)
-            self.generateSubTree(self.modificationTree,depth,nodeNum,xValue,iteration)
+        if currentNode == None:
+            if areWeGenerateFullTree == True:
+                self.modificationTree = Node(Type.FIRST,0)
+                self.modificationTree.child1 = self.generateRandomNode(depth,xValue)
+                self.generateSubTree(self.modificationTree.child1,depth,nodeNum,xValue)
+            else:
+                self.modificationTree = self.generateRandomNode(depth,xValue)
+                self.generateSubTree(self.modificationTree,depth,nodeNum,xValue)
 
 
 
         if currentNode != None and currentNode.type == Type.TRIGONOMETRY:
             currentNode.child1 = self.generateRandomNode(depth-1,xValue)
-            self.generateSubTree(currentNode.child1,depth-1,nodeNum-1,xValue,iteration+1)
+            self.generateSubTree(currentNode.child1,depth-1,nodeNum-1,xValue)
 
 
         if currentNode != None and currentNode.type == Type.OPERATOR:
@@ -138,18 +208,11 @@ class Tree:
             depthForGenerating = depthFirst
             if depthFirst > nodeNum:
                 depthForGenerating = nodeNum
-            # print(str(depthForGenerating) + " iter:" + str(iteration))
             currentNode.child1 = self.generateRandomNode(depthForGenerating,xValue)
-            self.generateSubTree(currentNode.child1,depthFirst,nodeNum-1,xValue,iteration+1)
+            self.generateSubTree(currentNode.child1,depthFirst,nodeNum-1,xValue)
 
             depthForGenerating = depthSecond
             if depthSecond > nodeNum:
                 depthForGenerating = nodeNum
-            # print(str(depthForGenerating) + " iter:" + str(iteration))
             currentNode.child2 = self.generateRandomNode(depthForGenerating,xValue)
-            self.generateSubTree(currentNode.child2,nodeNum-1,nodeNum-1,xValue,iteration+1)
-
-
-
-
-
+            self.generateSubTree(currentNode.child2,nodeNum-1,nodeNum-1,xValue)
